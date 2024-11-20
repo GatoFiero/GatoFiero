b499429f28194fa2760d3d11284305a29849ba79
GatoFiero <bababird9@gmail.com>
2024-11-20 16:02:42 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
+// scripts.js
+
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.querySelector('.menu-toggle');
     const navigationMenu = document.querySelector('.navigation-menu');
 
     menuToggle.addEventListener('click', () => {
-        const isExpanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
+        const isExpanded = menuToggle.getAttribute('aria-expanded') === 'true';
         menuToggle.setAttribute('aria-expanded', !isExpanded);
         navigationMenu.classList.toggle('active');
     });
