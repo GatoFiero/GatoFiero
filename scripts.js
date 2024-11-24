b2815c2aa7e84e2e9f3ff3fc2db1498f769d4cc9
GatoFiero <bababird9@gmail.com>
2024-11-24 14:21:56 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,4 +9,19 @@ document.addEventListener('DOMContentLoaded', () => {
         menuToggle.setAttribute('aria-expanded', !isExpanded);
         navigationMenu.classList.toggle('active');
     });
+
+    // Press Image Expansion
+    const pressArticle = document.getElementById('press-article');
+    const expandedImgContainer = document.createElement('div');
+    expandedImgContainer.id = 'expanded-img';
+    expandedImgContainer.innerHTML = `<img src="${pressArticle.src}" alt="${pressArticle.alt}">`;
+    document.body.appendChild(expandedImgContainer);
+
+    pressArticle.addEventListener('click', () => {
+        expandedImgContainer.style.display = 'flex';
+    });
+
+    expandedImgContainer.addEventListener('click', () => {
+        expandedImgContainer.style.display = 'none';
+    });
 });
